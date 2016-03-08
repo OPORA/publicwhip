97b149914c138253682b04f4acef74308fa42b58
Luke Bacon <luke@equivalentideas.com>
2016-03-08 17:10:28 +1100

    Add very basic test for the ukraine data loader task
    I'm adding some simple tests ahead of changing the implementation of the task
    to use the EveryPolitician gem for importing data stored there.

---
+++ spec/lib/data_loader/ukraine/people_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe DataLoader::Ukraine::People do
+  describe ".load!" do
+    subject(:load_data) do
+      VCR.use_cassette('everypolitician') do
+        DataLoader::Ukraine::People.new.load!
+      end
+    end
+
+    it "creates new people" do
+      load_data
+
+      expect(Person.count).to eq 443
+    end
+
+    it "extracts the person’s rada id" do
+      load_data
+
+      expect(Person.first.id).to eq 386
+    end
+
+    it "creates memberships for the people" do
+      load_data
+
+      expect(Person.first.members.count).to eq 1
+    end
+  end
+end
