bf66475d57dca19dacf3205e632adeb1c2b43cec
Luke Bacon <luke@equivalentideas.com>
2016-03-09 11:49:57 +1100

    Duplicate method ahead of changing implementation

--- spec/lib/data_loader/ukraine/people_spec.rb
+++ spec/lib/data_loader/ukraine/people_spec.rb
@@ -26,4 +26,30 @@ describe DataLoader::Ukraine::People do
       expect(Person.first.members.count).to eq 1
     end
   end
+
+  describe ".load_using_everypolitician_gem!" do
+    subject(:load_data) do
+      VCR.use_cassette('everypolitician') do
+        DataLoader::Ukraine::People.new.load!
+      end
+    end
+
+    it "creates new people" do
+      load_data
+
+      expect(Person.count).to eq 443
+    end
+
+    it "extracts the person’s rada id" do
+      load_data
+
+      expect(Person.first.id).to eq 386
+    end
+
+    it "creates memberships for the people" do
+      load_data
+
+      expect(Person.first.members.count).to eq 1
+    end
+  end
 end
