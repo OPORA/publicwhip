db29b784e4b0fed9ea71c7bb25117120ed55864b
Luke Bacon <luke@equivalentideas.com>
2016-03-15 11:39:00 +1100

    Add tests for the vote_event loader
    This is so we can change the implementation and be sure it still works.

--- spec/lib/data_loader/ukraine/vote_events_spec.rb
+++ spec/lib/data_loader/ukraine/vote_events_spec.rb
@@ -30,6 +30,58 @@ describe DataLoader::Ukraine::VoteEvents do
     end
   end
 
+  describe "#load!" do
+    before :each do
+      # Fix the Database Cleaner setup so this isn’t required
+      Member.delete_all
+      Person.delete_all
+
+      person1 = create(:person, id: 123)
+      person2 = create(:person, id: 456)
+      create(:member, first_name: "Jane", person: person1)
+      create(:member, first_name: "Ming", person: person2)
+    end
+
+    subject(:load_data) do
+      path = RSpec.configuration.fixture_path + "/popolo_vote_events.json"
+      DataLoader::Ukraine::VoteEvents.new(nil, path).load!
+    end
+
+    it "creates divisions" do
+      load_data
+
+      expect(Division.count).to eq 2
+    end
+
+    it "creates votes" do
+      load_data
+
+      expect(Vote.count).to eq 3
+    end
+
+    it "assigns votes to divisions" do
+      load_data
+
+      expect(Division.find(6064).votes.count).to eq 2
+    end
+
+    it "assigns votes to members" do
+      member_who_voted = Person.find(456).latest_member
+
+      load_data
+
+      expect(Division.find(6063).vote_for(member_who_voted)).to eq "aye"
+    end
+
+    it "creates bills" do
+      expected_bill_name = "Проект Закону про ратифікацію Протоколу про сталий транспорт до Рамкової конвенції про охорону та сталий розвиток Карпат"
+
+      load_data
+
+      expect(Bill.find_by_official_id("0081").title).to eq expected_bill_name
+    end
+  end
+
   describe "#popolo_to_publicwhip_vote" do
     subject(:data_loader) { DataLoader::Ukraine::VoteEvents.new() }
 
